ce00507e29398a085bf36c62285a55a1a64d2fda
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2023-11-30 13:52:07 -0600

    create hello page, start work on weather page

---
+++ app/controllers/example_pages_controller.rb
@@ -0,0 +1,14 @@
+class ExamplePagesController < ApplicationController
+
+  def hello_method
+    render json: {message: 'hello'}
+  end
+
+  def weather
+    render json: {message:
+      return "what city:"
+      city = gets.chomp
+    }
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/hello", controller: "example_pages", action: "hello_method"
+  get "/weather", controller: "example_pages", action: "weather"
 end
